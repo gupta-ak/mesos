21d5063622a16c3b24c4627c111d9d3d9e9f1657
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2018-03-20 20:22:49 -0700

    Windows: Removed `FD_CRT` from `WindowsFD` abstraction.
    After all the CRT APIs were replaced with Windows APIs, we no longer
    needed to support the semantics of an `int` file descriptor in
    general (in the sense of opening a CRT handle that's associated with
    the actual kernel object for the given `HANDLE`). There are specific
    use cases (usually third-party code) which still require a CRT
    int-like file descriptor, which the `crt()` function explicitly
    allocates (this allocation used to be done in the constructor).
    
    Thus the entire `FD_CRT` type was removed from the `WindowsFD`
    abstraction. It still acts like an `int` in the sense that it can be
    constructed from one and compared to one. However, construction via
    `int` only supports the standard file descriptors 0, 1, and 2 for
    `stdin`, `stdout`, and `stderr`. Any other construction creates an
    `int_fd` which holds an `INVALID_HANDLE_VALUE`. When being compared to
    an `int`, the abstraction simply returns -1 if it is invalid (based on
    the result of the `is_valid()` method) or 0 if it is valid. This is to
    support the semantics of checking validity by something like
    `if (fd < 0)` or `if (fd == -1)`.
    
    With the deletion of the `FD_CRT` type from `WindowsFD`, all the Stout
    APIs that switched on the type were simplified, with the last of the
    CRT code deleted.
    
    Thanks to the introduction of the private `int get_valid()` function,
    and the removal of the `FD_CRT` type, the comparison operators became
    much simpler.
    
    Several unit tests in the `FsTest` suite became cross-platform, with
    the `Close` test being simplified to test against an `int_fd`.
    
    Review: https://reviews.apache.org/r/66437

--- 3rdparty/stout/include/stout/os/windows/fd.hpp
+++ 3rdparty/stout/include/stout/os/windows/fd.hpp
@@ -13,6 +13,9 @@
 #ifndef __STOUT_OS_WINDOWS_FD_HPP__
 #define __STOUT_OS_WINDOWS_FD_HPP__
 
+#include <io.h> // For `_open_osfhandle`.
+#include <fcntl.h> // For `O_RDWR`.
+
 #include <array>
 #include <memory>
 #include <ostream>
@@ -37,9 +40,8 @@ namespace os {
 //   Try<int_fd> fd = os::open(...);
 //
 // The `WindowsFD` constructs off one of:
-//   (1) `int` - from the WinCRT API
-//   (2) `HANDLE` - from the Win32 API
-//   (3) `SOCKET` - from the WinSock API
+//   (1) `HANDLE` - from the Win32 API
+//   (2) `SOCKET` - from the WinSock API
 //
 // The `os::*` functions then take an instance of `WindowsFD`, examines
 // the state and dispatches to the appropriate API.
@@ -47,21 +49,11 @@ namespace os {
 class WindowsFD
 {
 public:
-  enum Type
+  enum class Type
   {
-    FD_CRT,
-    FD_HANDLE,
-    FD_SOCKET
+    HANDLE,
+    SOCKET
   };
-
-  WindowsFD() = default;
-
-  WindowsFD(int crt)
-    : type_(FD_CRT),
-      crt_(crt),
-      handle_(
-          crt < 0 ? INVALID_HANDLE_VALUE
-                  : reinterpret_cast<HANDLE>(::_get_osfhandle(crt))) {}
 
   // IMPORTANT: The `HANDLE` here is expected to be file handles. Specifically,
   //            `HANDLE`s returned by file API such as `CreateFile`. There are
@@ -71,50 +63,74 @@ public:
   //            than `INVALID_HANDLE_VALUE`.
   // TODO(mpark): Consider adding a second parameter which tells us what the
   //              error values are.
-  WindowsFD(HANDLE handle)
-    : type_(FD_HANDLE),
-      crt_(
-          handle == INVALID_HANDLE_VALUE
-            ? -1
-            : ::_open_osfhandle(reinterpret_cast<intptr_t>(handle), O_RDWR)),
-      handle_(handle) {}
+  WindowsFD(HANDLE handle) : type_(Type::HANDLE), handle_(handle) {}
 
-  WindowsFD(SOCKET socket) : type_(FD_SOCKET), socket_(socket) {}
+  WindowsFD(SOCKET socket) : type_(Type::SOCKET), socket_(socket) {}
+
+  WindowsFD(int crt) : WindowsFD(INVALID_HANDLE_VALUE)
+  {
+    if (crt == 0) {
+      handle_ = ::GetStdHandle(STD_INPUT_HANDLE);
+    } else if (crt == 1) {
+      handle_ = ::GetStdHandle(STD_OUTPUT_HANDLE);
+    } else if (crt == 2) {
+      handle_ = ::GetStdHandle(STD_ERROR_HANDLE);
+    }
+  }
 
   // On Windows, libevent's `evutil_socket_t` is set to `intptr_t`.
-  WindowsFD(intptr_t socket)
-    : type_(FD_SOCKET),
-      socket_(static_cast<SOCKET>(socket)) {}
+  WindowsFD(intptr_t socket) : WindowsFD(static_cast<SOCKET>(socket)) {}
 
   WindowsFD(const WindowsFD&) = default;
   WindowsFD(WindowsFD&&) = default;
+
+  // Default construct with invalid handle semantics.
+  WindowsFD() : WindowsFD(INVALID_HANDLE_VALUE) {}
 
   ~WindowsFD() = default;
 
   WindowsFD& operator=(const WindowsFD&) = default;
   WindowsFD& operator=(WindowsFD&&) = default;
 
+  bool is_valid() const
+  {
+    switch (type()) {
+      case Type::HANDLE:
+        return handle_ != nullptr && handle_ != INVALID_HANDLE_VALUE;
+      case Type::SOCKET:
+        return socket_ != INVALID_SOCKET;
+      default:
+        return false;
+    }
+  }
+
+  // NOTE: This allocates a C run-time file descriptor and associates
+  // it with the handle. At this point, the `HANDLE` should no longer
+  // be closed via `CloseHandle`, but instead close the returned `int`
+  // with `_close`. This method should almost never be used, and
+  // exists only for compatibility with 3rdparty dependencies.
   int crt() const
   {
-    CHECK((type() == FD_CRT) || (type() == FD_HANDLE));
-    return crt_;
+    CHECK_EQ(Type::HANDLE, type());
+    return ::_open_osfhandle(reinterpret_cast<intptr_t>(handle_), O_RDWR);
   }
 
   operator HANDLE() const
   {
-    CHECK((type() == FD_CRT) || (type() == FD_HANDLE));
+    CHECK_EQ(Type::HANDLE, type());
     return handle_;
   }
 
   operator SOCKET() const
   {
-    CHECK_EQ(FD_SOCKET, type());
+    CHECK_EQ(Type::SOCKET, type());
     return socket_;
   }
 
+  // On Windows, libevent's `evutil_socket_t` is set to `intptr_t`.
   operator intptr_t() const
   {
-    CHECK_EQ(FD_SOCKET, type());
+    CHECK_EQ(Type::SOCKET, type());
     return static_cast<intptr_t>(socket_);
   }
 
@@ -123,149 +139,138 @@ public:
 private:
   Type type_;
 
-  union
-  {
-    // We keep both a CRT FD as well as a `HANDLE`
-    // regardless of whether we were constructed
-    // from a file or a handle.
-    //
-    // This is because once we request for a CRT FD
-    // from a `HANDLE`, we're required to close it
-    // via `_close`. If we were to do the conversion
-    // lazily upon request, the resulting CRT FD
-    // would be dangling.
-    struct
-    {
-      int crt_;
-      HANDLE handle_;
-    };
+  union {
+    HANDLE handle_;
     SOCKET socket_;
   };
+
+  // NOTE: This function is provided only for checking validity, thus
+  // it is private. It provides a view of a `WindowsFD` as an `int`.
+  //
+  // TODO(andschwa): Fix all uses of this conversion to use `is_valid`
+  // directly instead, then remove the comparison operators. This
+  // would require writing an `int_fd` class for POSIX too, instead of
+  // just using `int`.
+  int get_valid() const
+  {
+    if (is_valid()) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+  // NOTE: These operators are used soley to support checking a
+  // `WindowsFD` against e.g. -1 or 0 for validity. Nothing else
+  // should have access to `get_valid()`.
+  friend bool operator<(int left, const WindowsFD& right);
+  friend bool operator<(const WindowsFD& left, int right);
+  friend bool operator>(int left, const WindowsFD& right);
+  friend bool operator>(const WindowsFD& left, int right);
+  friend bool operator<=(int left, const WindowsFD& right);
+  friend bool operator<=(const WindowsFD& left, int right);
+  friend bool operator>=(int left, const WindowsFD& right);
+  friend bool operator>=(const WindowsFD& left, int right);
+  friend bool operator==(int left, const WindowsFD& right);
+  friend bool operator==(const WindowsFD& left, int right);
+  friend bool operator!=(int left, const WindowsFD& right);
+  friend bool operator!=(const WindowsFD& left, int right);
 };
+
+
+inline std::ostream& operator<<(std::ostream& stream, const WindowsFD::Type& fd)
+{
+  switch (fd) {
+    case WindowsFD::Type::HANDLE: {
+      stream << "WindowsFD::Type::HANDLE";
+      return stream;
+    }
+    case WindowsFD::Type::SOCKET: {
+      stream << "WindowsFD::Type::SOCKET";
+      return stream;
+    }
+    default: {
+      stream << "WindowsFD::Type::UNKNOWN";
+      return stream;
+    }
+  }
+}
 
 
 inline std::ostream& operator<<(std::ostream& stream, const WindowsFD& fd)
 {
+  stream << fd.type() << ": ";
   switch (fd.type()) {
-    case WindowsFD::FD_CRT: {
-      stream << fd.crt();
-      break;
-    }
-    case WindowsFD::FD_HANDLE: {
+    case WindowsFD::Type::HANDLE: {
       stream << static_cast<HANDLE>(fd);
-      break;
+      return stream;
     }
-    case WindowsFD::FD_SOCKET: {
+    case WindowsFD::Type::SOCKET: {
       stream << static_cast<SOCKET>(fd);
-      break;
+      return stream;
+    }
+    default: {
+      stream << "UNKNOWN";
+      return stream;
     }
   }
-  return stream;
 }
 
 
-// The complexity in this function is due to our effort in trying to support the
-// cases where file descriptors are compared as an `int` on POSIX. For example,
-// we use expressions such as `fd < 0` to check for validity.
-// TODO(mpark): Consider introducing an `is_valid` function for `int_fd`.
+// NOTE: The following operators implement all the comparisons
+// possible between two `WindowsFD` types or a `WindowsFD` type and an
+// `int`. The point of this is that the `WindowsFD` type must act like
+// an `int` for compatibility reasons (e.g. checking validity through
+// `fd < 0`), without actually being castable to an `int` to avoid
+// ambiguous types.
 inline bool operator<(const WindowsFD& left, const WindowsFD& right)
 {
-  // In general, when compared against a `WindowsFD` in the `FD_CRT`, we map
-  // `INVALID_HANDLE_VALUE` and `INVALID_SOCKET` to `-1` before performing the
-  // comparison. The check for `< 0` followed by cast to `HANDLE` or `SOCKET` is
-  // due to the fact that `HANDLE` and `SOCKET` are both unsigned.
+  // These comparisons must come first because a default-constructed
+  // `WindowsFD` is invalid, but has `HANDLE` type, and a `WindowFD`
+  // constructed from `-1` is invalid (and has `HANDLE` type), but
+  // should be comparable against both `HANDLE` and `SOCKET` types.
+
+  // -1 < -1
+  if (!left.is_valid() && !right.is_valid()) {
+    return false;
+  }
+
+  // -1 < N_0
+  if (!left.is_valid() && right.is_valid()) {
+    return true;
+  }
+
+  // N_0 < -1
+  if (left.is_valid() && !right.is_valid()) {
+    return false;
+  }
+
+  // At this point, both `left` and `right` are valid, so the
+  // comparison only makes sense if they are of the same type.
+  CHECK_EQ(left.type(), right.type());
+
   switch (left.type()) {
-    case WindowsFD::FD_CRT: {
-      switch (right.type()) {
-        case WindowsFD::FD_CRT: {
-          return left.crt() < right.crt();
-        }
-        case WindowsFD::FD_HANDLE: {
-          if (static_cast<HANDLE>(right) == INVALID_HANDLE_VALUE) {
-            return left.crt() < -1;
-          }
-          if (left.crt() < 0) {
-            return true;
-          }
-#pragma warning(push)
-#pragma warning(disable : 4312)
-          // Disable `int`-to-`HANDLE` compiler warning. This is safe to do,
-          // see comment above.
-          return reinterpret_cast<HANDLE>(left.crt()) <
-                 static_cast<HANDLE>(right);
-#pragma warning(pop)
-        }
-        case WindowsFD::FD_SOCKET: {
-          if (static_cast<SOCKET>(right) == INVALID_SOCKET) {
-            return left.crt() < -1;
-          }
-          if (left.crt() < 0) {
-            return true;
-          }
-          return static_cast<SOCKET>(left.crt()) < static_cast<SOCKET>(right);
-        }
-      }
+    case WindowsFD::Type::HANDLE: {
+      return static_cast<HANDLE>(left) < static_cast<HANDLE>(right);
     }
-    case WindowsFD::FD_HANDLE: {
-      switch (right.type()) {
-        case WindowsFD::FD_CRT: {
-          if (static_cast<HANDLE>(left) == INVALID_HANDLE_VALUE) {
-            return -1 < right.crt();
-          }
-          if (right.crt() < 0) {
-            return false;
-          }
-#pragma warning(push)
-#pragma warning(disable : 4312)
-          // Disable `int`-to-`HANDLE` compiler warning. This is safe to do,
-          // see comment above.
-          return static_cast<HANDLE>(left) <
-                 reinterpret_cast<HANDLE>(right.crt());
-#pragma warning(pop)
-        }
-        case WindowsFD::FD_HANDLE: {
-          return static_cast<HANDLE>(left) < static_cast<HANDLE>(right);
-        }
-        case WindowsFD::FD_SOCKET: {
-          return static_cast<HANDLE>(left) <
-                 reinterpret_cast<HANDLE>(static_cast<SOCKET>(right));
-        }
-      }
-    }
-    case WindowsFD::FD_SOCKET: {
-      switch (right.type()) {
-        case WindowsFD::FD_CRT: {
-          if (static_cast<SOCKET>(left) == INVALID_SOCKET) {
-            return -1 < right.crt();
-          }
-          if (right.crt() < 0) {
-            return false;
-          }
-          return static_cast<SOCKET>(left) < static_cast<SOCKET>(right.crt());
-        }
-        case WindowsFD::FD_HANDLE: {
-          return reinterpret_cast<HANDLE>(static_cast<SOCKET>(left)) <
-                 static_cast<HANDLE>(right);
-        }
-        case WindowsFD::FD_SOCKET: {
-          return static_cast<SOCKET>(left) < static_cast<SOCKET>(right);
-        }
-      }
+    case WindowsFD::Type::SOCKET: {
+      return static_cast<SOCKET>(left) < static_cast<SOCKET>(right);
     }
   }
+
   UNREACHABLE();
 }
 
 
 inline bool operator<(int left, const WindowsFD& right)
 {
-  return WindowsFD(left) < right;
+  return left < right.get_valid();
 }
 
 
 inline bool operator<(const WindowsFD& left, int right)
 {
-  return left < WindowsFD(right);
+  return left.get_valid() < right;
 }
 
 
@@ -277,13 +282,13 @@ inline bool operator>(const WindowsFD& left, const WindowsFD& right)
 
 inline bool operator>(int left, const WindowsFD& right)
 {
-  return WindowsFD(left) > right;
+  return left > right.get_valid();
 }
 
 
 inline bool operator>(const WindowsFD& left, int right)
 {
-  return left > WindowsFD(right);
+  return left.get_valid() > right;
 }
 
 
@@ -295,13 +300,13 @@ inline bool operator<=(const WindowsFD& left, const WindowsFD& right)
 
 inline bool operator<=(int left, const WindowsFD& right)
 {
-  return WindowsFD(left) <= right;
+  return left <= right.get_valid();
 }
 
 
 inline bool operator<=(const WindowsFD& left, int right)
 {
-  return left <= WindowsFD(right);
+  return left.get_valid() <= right;
 }
 
 
@@ -313,118 +318,52 @@ inline bool operator>=(const WindowsFD& left, const WindowsFD& right)
 
 inline bool operator>=(int left, const WindowsFD& right)
 {
-  return WindowsFD(left) >= right;
+  return left >= right.get_valid();
 }
 
 
 inline bool operator>=(const WindowsFD& left, int right)
 {
-  return left >= WindowsFD(right);
+  return left.get_valid() >= right;
 }
 
 
-// The complexity in this function is due to our effort in trying to support the
-// cases where file descriptors are compared as an `int` on POSIX. For example,
-// we use expressions such as `fd != -1` to check for validity.
-// TODO(mpark): Consider introducing an `is_valid` function for `int_fd`.
 inline bool operator==(const WindowsFD& left, const WindowsFD& right)
 {
-  // In general, when compared against a `WindowsFD` in the `FD_CRT`, we map
-  // `INVALID_HANDLE_VALUE` and `INVALID_SOCKET` to `-1` before performing the
-  // comparison. The check for `< 0` followed by cast to `HANDLE` or `SOCKET` is
-  // due to the fact that `HANDLE` and `SOCKET` are both unsigned.
+  // This is `true` even if the types mismatch because we want
+  // `WindowsFD(-1)` to compare as equivalent to an invalid `HANDLE`
+  // or `SOCKET`, even though it is technically of type `HANDLE`.
+  if (!left.is_valid() && !right.is_valid()) {
+    return true;
+  }
+
+  // Otherwise mismatched types are not equivalent.
+  if (left.type() != right.type()) {
+    return false;
+  }
+
   switch (left.type()) {
-    case WindowsFD::FD_CRT: {
-      switch (right.type()) {
-        case WindowsFD::FD_CRT: {
-          return left.crt() == right.crt();
-        }
-        case WindowsFD::FD_HANDLE: {
-          if (static_cast<HANDLE>(right) == INVALID_HANDLE_VALUE) {
-            return left.crt() == -1;
-          }
-          if (left.crt() < 0) {
-            return false;
-          }
-#pragma warning(push)
-#pragma warning(disable : 4312)
-          // Disable `int`-to-`HANDLE` compiler warning. This is safe to do,
-          // see comment above.
-          return reinterpret_cast<HANDLE>(left.crt()) ==
-                 static_cast<HANDLE>(right);
-#pragma warning(pop)
-        }
-        case WindowsFD::FD_SOCKET: {
-          if (static_cast<SOCKET>(right) == INVALID_SOCKET) {
-            return left.crt() == -1;
-          }
-          if (left.crt() < 0) {
-            return false;
-          }
-          return static_cast<SOCKET>(left.crt()) == static_cast<SOCKET>(right);
-        }
-      }
+    case WindowsFD::Type::HANDLE: {
+      return static_cast<HANDLE>(left) == static_cast<HANDLE>(right);
     }
-    case WindowsFD::FD_HANDLE: {
-      switch (right.type()) {
-        case WindowsFD::FD_CRT: {
-          if (static_cast<HANDLE>(left) == INVALID_HANDLE_VALUE) {
-            return -1 == right.crt();
-          }
-          if (right.crt() < 0) {
-            return false;
-          }
-#pragma warning(push)
-#pragma warning(disable : 4312)
-          // Disable `int`-to-`HANDLE` compiler warning. This is safe to do,
-          // see comment above.
-          return static_cast<HANDLE>(left) ==
-                 reinterpret_cast<HANDLE>(right.crt());
-#pragma warning(pop)
-        }
-        case WindowsFD::FD_HANDLE: {
-          return static_cast<HANDLE>(left) == static_cast<HANDLE>(right);
-        }
-        case WindowsFD::FD_SOCKET: {
-          return static_cast<HANDLE>(left) ==
-                 reinterpret_cast<HANDLE>(static_cast<SOCKET>(right));
-        }
-      }
-    }
-    case WindowsFD::FD_SOCKET: {
-      switch (right.type()) {
-        case WindowsFD::FD_CRT: {
-          if (static_cast<SOCKET>(left) == INVALID_SOCKET) {
-            return -1 == right.crt();
-          }
-          if (right.crt() < 0) {
-            return false;
-          }
-          return static_cast<SOCKET>(left) == static_cast<SOCKET>(right.crt());
-        }
-        case WindowsFD::FD_HANDLE: {
-          return reinterpret_cast<HANDLE>(static_cast<SOCKET>(left)) ==
-                 static_cast<HANDLE>(right);
-        }
-        case WindowsFD::FD_SOCKET: {
-          return static_cast<SOCKET>(left) == static_cast<SOCKET>(right);
-        }
-      }
+    case WindowsFD::Type::SOCKET: {
+      return static_cast<SOCKET>(left) == static_cast<SOCKET>(right);
     }
   }
+
   UNREACHABLE();
 }
 
 
 inline bool operator==(int left, const WindowsFD& right)
 {
-  return WindowsFD(left) == right;
+  return left == right.get_valid();
 }
 
 
 inline bool operator==(const WindowsFD& left, int right)
 {
-  return left == WindowsFD(right);
+  return left.get_valid() == right;
 }
 
 
@@ -436,13 +375,13 @@ inline bool operator!=(const WindowsFD& left, const WindowsFD& right)
 
 inline bool operator!=(int left, const WindowsFD& right)
 {
-  return WindowsFD(left) != right;
+  return left != right.get_valid();
 }
 
 
 inline bool operator!=(const WindowsFD& left, int right)
 {
-  return left != WindowsFD(right);
+  return left.get_valid() != right;
 }
 
 } // namespace os {
@@ -458,16 +397,14 @@ struct hash<os::WindowsFD>
   result_type operator()(const argument_type& fd) const
   {
     switch (fd.type()) {
-      case os::WindowsFD::FD_CRT: {
-        return static_cast<result_type>(fd.crt());
-      }
-      case os::WindowsFD::FD_HANDLE: {
+      case os::WindowsFD::Type::HANDLE: {
         return reinterpret_cast<result_type>(static_cast<HANDLE>(fd));
       }
-      case os::WindowsFD::FD_SOCKET: {
+      case os::WindowsFD::Type::SOCKET: {
         return static_cast<result_type>(static_cast<SOCKET>(fd));
       }
     }
+
     UNREACHABLE();
   }
 };
